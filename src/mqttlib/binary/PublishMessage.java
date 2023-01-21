2ac71f00f40b203c14a3ccdf08f99233aa3b0bc7
Florent Goux <florent.goux@telecom-paris.fr>
2023-01-21 14:48:02 +0100

    Update PublishMessage.java

--- src/mqttlib/binary/PublishMessage.java
+++ src/mqttlib/binary/PublishMessage.java
@@ -1,6 +1,23 @@
 package mqttlib.binary;
 
-public class PublishMessage
+import java.io.IOException;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.WritableByteChannel;
+
+public class PublishMessage extends MqttMessage
 {
+
+    @Override
+    public void writeTo(final WritableByteChannel channel) throws IOException
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void initialise(final ReadableByteChannel byteChannel, final int remainingLength) throws IOException
+    {
+        throw new RuntimeException("not implemented"); // not needed for 6.2
+    }
 
 }
